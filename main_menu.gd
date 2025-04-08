619d1bcf6531af424346cfb0fab457d2746fda06
Andrey Guvir <guvirandrey2007@gmail.com>
2025-04-08 23:41:33 +0200

    First commit

---
+++ main_menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://root.tscn")
+
+
+func _on_options_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://options_main_menu.tscn")
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
